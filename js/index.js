49cabc4b3fb93c9750e518136bdfdf917dc27b54
Leslie Thompson <lcrthompson@gmail.com>
2019-03-19 14:23:54 -0700

    buttons

--- js/index.js
+++ js/index.js
@@ -1,13 +1,25 @@
 // Your code goes here
+//NAV CLICK
 const navText = Array.from(document.getElementsByClassName("nav-link"));
-console.log(navText);
+// console.log(navText);
 
 // navText[0].addEventListener("click", ev => {
 //   console.log("nav clicked");
 // });
-
 navText.forEach(link => {
   link.addEventListener("click", ev => {
     link.style.color = "lightblue";
+  });
+});
+
+// IMG FOCUS
+
+const btn = Array.from(document.getElementsByClassName("btn"));
+console.log(btn);
+
+btn.forEach(btn => {
+  btn.addEventListener("click", ev => {
+    btn.style.color = "lightblue";
+    btn.style.backgroundColor = "white";
   });
 });
