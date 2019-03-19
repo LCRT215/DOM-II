40fea4016d9d92a4670de454e7276dcaeb4d4a49
Leslie Thompson <lcrthompson@gmail.com>
2019-03-19 14:06:01 -0700

    click nav

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+const navText = Array.from(document.getElementsByClassName("nav-link"));
+console.log(navText);
+
+// navText[0].addEventListener("click", ev => {
+//   console.log("nav clicked");
+// });
+
+navText.forEach(link => {
+  link.addEventListener("click", ev => {
+    link.style.color = "lightblue";
+  });
+});
